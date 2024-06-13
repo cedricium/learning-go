57d1ecc654403c8cbb89f893508615eb0ea0cf3f
Cedric Amaya <amaya.cedric@gmail.com>
2024-06-13 13:04:13 -0700

    Chapter 10 solution 2: v1.0.1

--- learning-go/ch10/main.go
+++ learning-go/ch10/main.go
@@ -1,5 +1,9 @@
+// Ch10 contains solutions to "Learning Go" book, Chapter 10.
 package ch10
 
+// Add adds two integers and returns the sum.
+// Follows the rules of addition as noted here:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
